14f9714c006c407f56ea604f064bab21112a4e72
kshuev1 <andreikshyev@gmail.com>
2026-06-09 23:07:40 +0300

    Add files via upload

---
+++ App/service/src/main/java/ru/netology/MyService.java
@@ -0,0 +1,26 @@
+package ru.netology;
+import ru.netology.Db;
+import ru.netology.DbSetting;
+import ru.netology.MyEntity;
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
